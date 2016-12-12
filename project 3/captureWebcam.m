78b75cb5a194719b86137087bf84d118bded65fe
Ernie <ernie0218e@gmail.com>
2016-12-12 10:42:59 +0800

    add webcam support

---
+++ project 3/captureWebcam.m
@@ -0,0 +1,22 @@
+clear;
+cam = webcam(1);
+
+for idx = 1:100
+   % Acquire a single image.
+   rgbImage = snapshot(cam);
+   
+   rgbImage = imresize(rgbImage, 0.5);
+
+   % Convert RGB to grayscale.
+   grayImage = rgb2gray(rgbImage);
+   
+   points = detectHarrisFeatures(grayImage);
+
+   % Display the image.
+   imshow(rgbImage);
+   hold on;
+   plot(points);
+   drawnow
+end
+
+clear;
